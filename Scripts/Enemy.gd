73498e450337a2446df03f0964c12ef3a8ca7648
BL-MSCH-C220-F19 <53633703+BL-MSCH-C220-F19@users.noreply.github.com>
2020-02-23 12:18:18 -0500

    Updates rotation and bullet behavior
    Sorry for the delay, if you remind me, I will discount the late pentaly by four days. Part of the problem you were having was relying too much on the rotation_degrees (which is managed by the physics engine) rather than calculating it yourself. I tried to make everything based on a global variable in the Game script, so you could rotate at a different angle than 90 if you choose.

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -5,8 +5,11 @@ export var score = 10
 export var speed = 2.0
 export var move_probability = 0.05
 export var fire_probability = 0.5
+var choice = randi()%4
 
 onready var EnemyBullet = load("res://Scenes/EnemyBullet.tscn")
+
+onready var rot_seg = get_node("/root/Game").rotate_segments
 
 
 var ready = false
@@ -18,15 +21,8 @@ func get_new_position():
 	new_position.x = randi() % int(VP.x)
 	new_position.y = min(randi() % int(VP.y), int(VP.y) - limit_y)
 	$Tween.interpolate_property(self, "position", position, new_position, speed, Tween.TRANS_EXPO, Tween.EASE_IN_OUT)
-	var choice = randi()%4+1
-	if choice == 1:
-		rotation_degrees = 0
-	elif choice == 2:
-		rotation_degrees = 90
-	elif choice == 3:
-		rotation_degrees = 180
-	else:
-		rotation_degrees = 270
+	rotation_degrees = choice * (360 / rot_seg)
+	
 	ready = true
 
 func die():
@@ -50,6 +46,9 @@ func _on_Timer_timeout():
 	if randf() < fire_probability:
 		var b = EnemyBullet.instance()
 		b.position = position
-		b.position.y += 25
+		var vel_rot = deg2rad((choice * (360 / rot_seg)) - 90)
+		b.position.x += cos(vel_rot) * 25
+		b.position.y += sin(vel_rot) * 25
+		b.rot = choice
 		get_node("/root/Game/Enemy Bullets").add_child(b)
 		
---
+++ Scripts/Game.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+export var rotate_segments = 4
+
+func _ready():
+	pass # Replace with function body.
+
